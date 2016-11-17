ba318f8af7a16e18672e34633cb0d37f13812441
bradymat <bradymat@gmail.com>
2016-11-17 16:31:50 +1300

    Made a really awesome website yo

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var morphdom = require('morphdom')
 
 var reducer = require('./reducer')
 var productsTemplate = require('./views/products')
+var cartTemplate = require('./views/cart')
 
 var main = document.querySelector('main')
 var app = document.createElement('div')
@@ -11,23 +12,27 @@ main.appendChild(app)
 
 var initialState = {
   products: [
-    {id: 1, name: 'The Name of the Wind', price: 12.50, details: 'Kvothe is cool'},
-    {id: 2, name: 'Firefall', price: 11.29, details: 'Aliens are scary. Space vampires'},
-    {id: 3, name: 'Kingdom of fear', price: 34.33, details: 'Song of the sausage creature'}
+    {id: 1, name: 'The Punisher', price: 120.50, details: 'For wen some1s been real naughty'},
+    {id: 2, name: 'Big Stan', price: 1100.29, details: 'Really Robs your Schneider'},
+    {id: 3, name: 'Kingdom of Rear', price: 69.69, details: 'Song of the Sausage Creature'},
+    {id: 4, name: 'Bangers and Mash', price: 4.20, details: 'A full english breakfast'}
   ],
-  cart: {
-    1: 2 // There are two items with id 1 in the cart
-  }
+  cart: { }
 }
 
-var store = redux.createStore(/*???*/)
+var store = redux.createStore(reducer, initialState)
 
-store.subscribe(/*???*/)
+store.subscribe(function () {
+  var state = store.getState()
+  var view = render(state, store.dispatch)
+  morphdom(app, view)
+})
 
-store.dispatch({type: 'INIT'}) //triggers an initial render
+store.dispatch({type: 'INIT'})
 
 function render (state, dispatch) {
   return h('div#app', {}, [
-    productsTemplate(/*???*/)
+    productsTemplate(state.products, dispatch),
+    cartTemplate(state, dispatch)
   ])
 }
